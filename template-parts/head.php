c7bd0237b8f53a82918f3d58fb5ba6f3e3f027ae
Robbert Vermeulen <robbertvermeulen@gmail.com>
2020-01-22 16:54:46 +0100

    Change viewport meta scale

--- template-parts/head.php
+++ template-parts/head.php
@@ -14,7 +14,7 @@
       <?php } ?>
 
       <meta charset="<?php bloginfo( 'charset' ); ?>">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
       <title><?php wp_title( false ); ?></title>
       <?php wp_head(); ?>
    </head>
