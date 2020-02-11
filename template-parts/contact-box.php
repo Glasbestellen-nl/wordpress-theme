54d3966f70f2a417386b73cf516ac23b92525e48
Robbert Vermeulen <robbertvermeulen@gmail.com>
2020-02-11 10:25:21 +0100

    Add contact box between paragraphs

---
+++ template-parts/contact-box.php
@@ -0,0 +1,9 @@
+<div class="card">
+   <div class="card__body text-center">
+      <h2 class="h2 card__title"><?php _e( 'Neem contact op', 'glasbestellen' ); ?></h2>
+      <div class="text space-below">
+         <p class="card__text"><?php _e( 'Bel of mail ons voor advies. Wij informeren u graag!', 'glasbestellen' ); ?></p>
+      </div>
+      <span class="btn btn--secondary btn--next js-popup-form" data-formtype="lead"><?php _e( 'Neem contact op', 'glasbestellen' ); ?></a>
+   </div>
+</div>
