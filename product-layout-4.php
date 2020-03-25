23e408ca390d76aee320b72f0c73c4f8c89f183c
Robbert Vermeulen <robbertvermeulen@gmail.com>
2020-03-25 15:57:12 +0100

    Match heights products product layout 4

--- product-layout-4.php
+++ product-layout-4.php
@@ -64,7 +64,7 @@ get_header(); ?>
                                        <img data-src="<?php echo get_the_post_thumbnail_url( get_the_id(), 'large' ); ?>" class="lazyload product-listing__image-img" alt="<?php echo get_post_meta( get_post_thumbnail_id(), '_wp_attachment_image_alt', true ); ?>">
                                     </a>
                                     <div class="product-listing__body">
-                                       <h2 class="h5"><a href="<?php the_permalink(); ?>" class="product-listing__title"><?php the_title(); ?></a></h2>
+                                       <h2 class="h5"><a href="<?php the_permalink(); ?>" class="product-listing__title" data-mh="product-listing-title"><?php the_title(); ?></a></h2>
                                        <div class="product-listing__info">
                                           <?php if ( ! empty( $settings['price'] ) ) { ?>
                                              <span class="product-listing__price"><?php echo sprintf( __( 'v.a. %s', 'glasbestellen' ), Money::display( $settings['price'] ) ); ?></span>
