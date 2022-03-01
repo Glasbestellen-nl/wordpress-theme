07c4505f3eea2b5aa06c2e261d072b9d2e31b615
Robbert Vermeulen <robbertvermeulen@gmail.com>
2022-03-01 09:58:45 +0100

    Activate trigger activecampaign purchase event hook

--- inc/webhook.php
+++ inc/webhook.php
@@ -107,19 +107,4 @@ function gb_trigger_activecampaign_purchase_events( $transaction, $cart ) {
       $category_ids[] = $category_id;
    }
 }
-// add_action( 'template_include', function() {
-//    $transaction = new Transaction( 2379 );
-//    $cart = new Cart( $transaction->get_items() );
-//    gb_trigger_activecampaign_purchase_events( $transaction, $cart );
-// });
-// add_action( 'gb_webhook_payment_success', 'gb_trigger_activecampaign_purchase_events' );
-
-// add_action( 'template_include', function() {
-//    $response = Active_Campaign_Wrapper::get_instance()->log_event(
-//       'robbertvermeulen@gmail.com',
-//       'configured_product_purchase',
-//       'startopstelling_117'
-//    );
-//    var_dump( $response );
-//    die;
-// });
+add_action( 'gb_webhook_payment_success', 'gb_trigger_activecampaign_purchase_events' );
