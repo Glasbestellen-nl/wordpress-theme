8da8ad455fdcb9c22778c31332a5937e47793f1a
Robbert Vermeulen <robbertvermeulen@Robberts-MacBook-Pro-2.local>
2022-03-29 16:52:13 +0200

    Create product templates

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -1,9 +1,67 @@
 <?php
-function mytheme_add_woocommerce_support() {
+/**
+ * Single product wrapper class
+ */
+function gb_single_product_wrapper_class() {
+    global $product;
+    $classes = [];
+    if ( 'configurable' == $product->get_type() ) {
+        $classes[] = 'configurator';
+        $classes[] = 'js-configurator';
+    }
+    $classes = apply_filters( 'gb_single_product_wrapper_class', $classes, $product );
+    return implode( ' ', $classes );
+}
+
+/**
+ * Adds configurable product type class
+ */
+function gb_register_configurable_product_type() {
+
+    class WC_Product_Configurable extends WC_Product_Simple {
+        public function __construct( $product ) {
+            $this->product_type = 'configurable';
+            parent::__construct( $product );
+        }
+
+        public function get_type(){
+            return 'configurable';
+        }
+    }
+}
+add_action( 'init', 'gb_register_configurable_product_type' );
+
+/**
+ * Adds configurable product type class
+ */
+function gb_load_product_type_class( $classname, $product_type ) {
+	if ( $product_type == 'configurable' ) {
+	    $classname = 'WC_Product_Configurable';
+	}
+	return $classname;
+}
+add_filter( 'woocommerce_product_class', 'gb_load_product_type_class', 10, 2);
+
+/**
+ * Adds configurable product type to selector
+ */
+function gb_add_configurable_product_type_selector( $types ){
+    $types['configurable'] = __( 'Configurable product' );
+    return $types;
+}
+add_filter( 'product_type_selector', 'gb_add_configurable_product_type_selector' );
+
+/**
+ * Enables woocommerce customization
+ */
+function gb_add_woocommerce_support() {
 	add_theme_support( 'woocommerce' );
 }
-add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+add_action( 'after_setup_theme', 'gb_add_woocommerce_support' );
 
+/**
+ * Adds rewrite rules for product and product cat (shop) with same base
+ */
 add_filter( 'rewrite_rules_array', function( $rules ) {
     $new_rules = [
         'producten/([^/]*?)/?$' => 'index.php?product_cat=$matches[1]',
---
+++ woocommerce/content-product.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The template for displaying product content within loops
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/content-product.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 3.6.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+global $product;
+
+// Ensure visibility.
+if ( empty( $product ) || ! $product->is_visible() ) {
+	return;
+}
+
+$product_count = ! empty( $product_count ) ? $product_count : 0;
+$static_price = get_field( 'static_price' );
+?>
+
+<div <?php wc_product_class( 'product-listing', $product ); ?>>
+   <a href="<?php the_permalink(); ?>" class="product-listing__image">
+      <img src="<?php echo get_the_post_thumbnail_url( get_the_id(), 'large' ); ?>" class="product-listing__image-img" alt="<?php echo get_post_meta( get_post_thumbnail_id(), '_wp_attachment_image_alt', true ); ?>">
+   </a>
+   <div class="product-listing__body">
+		<h3 class="h5"><a href="<?php the_permalink(); ?>" class="product-listing__title" data-mh="product-listing-title"><?php the_title(); ?></a></h3>
+      <div class="product-listing__info">
+         <span class="product-listing__price"><?php echo ( ( ! $static_price ) ? __( 'v.a.', 'glasbestellen' ) : '' ) . ' ' . wc_price( $product->get_price() ); ?></span>
+         <span class="product-listing__tax"><?php _e( 'Prijs incl. BTW.', 'glasbestellen' ); ?></span>
+         <span class="product-listing__shipping"><i class="fas fa-shipping-fast"></i> <?php _e( 'Gratis verzending', 'glasbestellen' ); ?></span>
+      </div>
+   </div>
+   <a href="<?php the_permalink(); ?>" class="product-listing__link-cover"></a>
+</div>
+
