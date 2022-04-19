96eb419860161453eabb869963491d9ccef13a35
Robbert Vermeulen <robbertvermeulen@Robberts-MacBook-Pro-2.local>
2022-04-19 10:32:49 +0200

    Check whether wc product extended class exists

--- class/WC_Product_Configurable.php
+++ class/WC_Product_Configurable.php
@@ -1,24 +1,28 @@
 <?php
-class WC_Product_Configurable extends WC_Product_Extended {
+if ( class_exists( 'WC_Product_Extended' ) ) {
 
-    public $price;
+    class WC_Product_Configurable extends WC_Product_Extended {
 
-    public function __construct( $product ) {
-        parent::__construct( $product );
-        $this->product_type = 'configurable';
-    }
+        public $price;
 
-    public function get_type(){
-        return 'configurable';
-    }
+        public function __construct( $product ) {
+            parent::__construct( $product );
+            $this->product_type = 'configurable';
+        }
 
-    public function get_configurator_id() {
-        $configurator_id = get_post_meta( $this->id, 'configurator', true );
-        return $configurator_id;
-    }
+        public function get_type(){
+            return 'configurable';
+        }
 
-    public function get_configurator() {
-        return gb_get_configurator( $this->get_configurator_id() );
+        public function get_configurator_id() {
+            $configurator_id = get_post_meta( $this->id, 'configurator', true );
+            return $configurator_id;
+        }
+
+        public function get_configurator() {
+            return gb_get_configurator( $this->get_configurator_id() );
+        }
+
     }
 
 }
