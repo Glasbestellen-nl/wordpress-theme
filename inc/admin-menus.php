04a25c63cfdd974fc4a99ea1697ad272e05a4862
Robbert Vermeulen <robbertvermeulen@Robberts-MacBook-Pro-2.local>
2022-05-04 11:42:16 +0200

    Meta description and title managable

--- inc/admin-menus.php
+++ inc/admin-menus.php
@@ -16,14 +16,43 @@ function gb_products_page_content_load() {
 	if ( isset( $_POST['page_content_submit'] ) )	 {
 
 		$data = array(
-			'title' 		=> $_POST['page_content_title'],
-			'content'	=> stripslashes_deep( $_POST['page_content_text'] ),
+			'title' 			=> $_POST['page_content_title'],
+			'content'			=> stripslashes_deep( $_POST['page_content_text'] ),
+			'meta_title' 		=> $_POST['page_meta_title'],
+			'meta_description'  => $_POST['page_meta_description']
 		);
 		update_option( 'product_post_type_content', $data );
 		wp_redirect( admin_url( 'edit.php?post_type=product&page=products-page-content&updated=true' ) );
 		exit;
 	}
 }
+
+/**
+ * Sets products archive meta title from options value
+ */
+function gb_products_archive_meta_title( $title ) {
+	if ( ! get_query_var( 'is_product_cat_archive' ) ) return;
+	$page_content = get_option( 'product_post_type_content' );
+	if ( ! empty( $page_content['meta_title'] ) ) {
+		$title = $page_content['meta_title'] . ' - ' . get_bloginfo( 'name' );
+	}
+	return $title;
+}
+add_filter( 'wpseo_title', 'gb_products_archive_meta_title' );
+
+/**
+ * Sets products archive meta description from options value
+ */
+function gb_products_archive_meta_description( $desc ) {
+	if ( ! get_query_var( 'is_product_cat_archive' ) ) return;
+	$page_content = get_option( 'product_post_type_content' );
+	if ( ! empty( $page_content['meta_description'] ) ) {
+		$desc = $page_content['meta_description'];
+	}
+	return $desc;
+}
+add_filter( 'wpseo_metadesc', 'gb_products_archive_meta_description' );
+
 
 /**
  * Products page content menu page
@@ -48,6 +77,16 @@ function gb_products_content_page() {
 				<tr>
 					<th><label for="page_content_title"><?php _e( 'Titel', 'glasbestellen' ); ?>:</label></th>
 					<td><input type="text" name="page_content_title" id="page_content_title" class="regular-text"<?php if ( ! empty( $data['title'] ) ) echo 'value="' . $data['title'] . '"'; ?>></td>
+				</tr>
+
+				<tr>
+					<th><label for="page_meta_title"><?php _e( 'Meta title', 'glasbestellen' ); ?>:</label></th>
+					<td><input type="text" name="page_meta_title" id="page_content_title" class="regular-text"<?php if ( ! empty( $data['meta_title'] ) ) echo 'value="' . $data['meta_title'] . '"'; ?>></td>
+				</tr>
+
+				<tr>
+					<th><label for="page_meta_description"><?php _e( 'Meta description', 'glasbestellen' ); ?>:</label></th>
+					<td><textarea rows="5" cols="50" name="page_meta_description"><?php echo ! empty( $data['meta_description'] ) ? $data['meta_description'] : ''; ?></textarea></td>
 				</tr>
 
 				<tr>
