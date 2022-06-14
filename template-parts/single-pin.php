e19494bf3b6d6936d8023b6211a573b41485a5d8
Robbert Vermeulen <robbertvermeulen@Robberts-MacBook-Pro-2.local>
2022-06-14 14:56:58 +0200

    Connect inspiration category with product cat via custom field

--- template-parts/single-pin.php
+++ template-parts/single-pin.php
@@ -1,3 +1,7 @@
+<?php
+$terms = get_the_terms( get_the_id(), 'inspiratie-categorie' );
+?>
+
 <div class="pin">
 
    <div class="row">
@@ -17,7 +21,7 @@
                <?php the_content(); ?>
             </div>
 
-            <?php if ( $terms = get_the_terms( get_the_id(), 'inspiratie-categorie' ) ) { ?>
+            <?php if ( $terms ) { ?>
                <div class="pin__tags taggers large-space-below">
                   <?php foreach ( $terms as $term ) { ?>
                      <a href="<?php echo get_term_link( $term->term_id ); ?>" class="tagger"><?php echo strtolower( $term->name ); ?></a>
@@ -28,8 +32,8 @@
             <div class="pin__buttons">
                <div class="row">
                   <?php
-                  if ( ! empty( $terms ) ) {
-                     if ( $product_cat_id = gb_get_product_id_by_string( $terms[0]->name ) ) { ?>
+                  if ( $terms ) {
+                     if ( $product_cat_id = get_field( 'product_cat', 'term_' . $terms[0]->term_id ) ) { ?>
                         <div class="col-12 col-lg-6">
                            <a href="<?php echo get_term_link( $product_cat_id, 'product_cat' ); ?>" class="btn btn--primary btn--block btn--next pin__button"><?php _e( 'Meer informatie', 'glasbestellen' ); ?></a>
                         </div>
