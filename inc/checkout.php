cca14e19d753ffc10381530861d01d34a074ce9c
Robbert Vermeulen <robbertvermeulen@Robberts-MacBook-Pro-2.local>
2022-06-16 09:42:36 +0200

    Remove order notes checkout field

---
+++ inc/checkout.php
@@ -0,0 +1,10 @@
+<?php
+// Removes Order Notes Title - Additional Information & Notes Field
+add_filter( 'woocommerce_enable_order_notes_field', '__return_false', 9999 );
+
+// Remove Order Notes Field
+function remove_order_notes( $fields ) {
+    unset ( $fields['order']['order_comments'] );
+    return $fields;
+}
+add_filter( 'woocommerce_checkout_fields' , 'remove_order_notes' );
