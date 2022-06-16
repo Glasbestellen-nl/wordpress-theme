04f1d63ed3895bb965187d68d4f70b3a510a96a5
Robbert Vermeulen <robbertvermeulen@Robberts-MacBook-Pro-2.local>
2022-06-16 11:28:40 +0200

    Change order button text

--- inc/cart.php
+++ inc/cart.php
@@ -1,12 +1,4 @@
 <?php
-// function gb_handle_get_cart_quantity() {
-//    global $woocommerce;
-//    echo $woocommerce->cart->cart_contents_count;
-//    wp_die();
-// }
-// add_action( 'wp_ajax_get_cart_quantity', 'gb_handle_get_cart_quantity' );
-// add_action( 'wp_ajax_nopriv_get_cart_quantity', 'gb_handle_get_cart_quantity' );
-
 /**
  * Show cart contents / total Ajax
  */
--- inc/checkout.php
+++ inc/checkout.php
@@ -1,10 +1,24 @@
 <?php
-function theme_wc_setup() {
+/**
+ * Changes "Place Order" Button text WooCommerce Checkout
+ */
+function bcc_woocommerce_button_text( $button_text ) {
+	return __( 'Bestellen en betalen', 'glasbestellen' );
+}
+add_filter( 'woocommerce_order_button_text', 'bcc_woocommerce_button_text' );
+
+/**
+ * Moves payment below billing fields
+ */
+function bcc_theme_wc_setup() {
     remove_action( 'woocommerce_checkout_order_review', 'woocommerce_checkout_payment', 20 );
     add_action( 'woocommerce_checkout_after_shipping', 'woocommerce_checkout_payment', 20 );
 }
-add_action( 'after_setup_theme', 'theme_wc_setup' );
+add_action( 'after_setup_theme', 'bcc_theme_wc_setup' );
 
+/**
+ * Adds heading above payment on checkout
+ */
 add_action( 'woocommerce_review_order_before_payment', function() {
     echo '<h3>' . __( 'Kies betaalmethode', 'glasbestellen' ) . '</h3>';
 });
