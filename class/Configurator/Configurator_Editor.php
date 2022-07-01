20c97c3dc8548e021703c61b69c094c0eaeaf175
Robbert Vermeulen <robbertvermeulen@Robberts-MacBook-Pro-2.local>
2022-07-01 15:11:47 +0200

    Update configigurator editor

--- class/Configurator/Configurator_Editor.php
+++ class/Configurator/Configurator_Editor.php
@@ -23,10 +23,12 @@ class Configurator_Editor {
     }
 
     public function add_option( string $step_id, Option $option ) {
+        $step_index = get_step_index_by_id( $step_id );
         $step = $this->get_step_by_id( $step_id );
         if ( ! $step ) return;
-        $step_options = $step['options'];
-        $step_options = [
+        $option_id = $this->generate_next_option_id( $step_id );
+        $step['options'] = [
+            'id'      => $option_id,
             'title'   => $option->get_title(),
             'price'   => $option->get_price(),
             'default' => $option->get_default(),
@@ -39,11 +41,13 @@ class Configurator_Editor {
                     $step_options['rules']['exclude'][] = [
                         'step'    => $rule->get_step_id(),
                         'options' => $rule->get_options(),
+                        'message' => sprintf( 'De combinatie %s en de keuze in stap %s is niet mogelijk. Verander a.u.b. een van de keuzes.', $option->get_title(), $this->get_step_title_by_id() )
                     ];
                     $other_step = $this->get_step_by_id( $rule->get_step_id() );
                     if ( $other_step ) {
                         foreach ( $rule->get_options() as $option_id ) {
-                            $other_step['options'][]['rules']['exclude'] = [
+                            $option_index = $this->get_option_index_by_id( $rule->get_step_id(), $option_id );
+                            $other_step['options'][$option_index]['rules']['exclude'] = [
                                 'step' => $step_id,
                                 'options' => [],
                             ]; 
@@ -52,15 +56,51 @@ class Configurator_Editor {
                 }
             }
         }
+        $this->_settings['steps'][$step_index] = $step;
+        return true;
     }
 
     public function get_step_index_by_id( string $step_id ) {
-
+        $steps = $this->_settings['steps'];
+        if ( empty( $steps ) ) return;
+        $step_index = false;
+        foreach ( $steps as $index => $step ) {
+            if ( $step['id'] == $step_id ) 
+                $step_index = $index;
+        }
+        return $index;
     }
 
-    public function get_step_by_id() {
+    public function get_step_by_id( string $step_id ) {
         $index = $this->get_step_index_by_id( $step_id );
         return ! empty( $this->_settings['steps'][$index] ) ? $this->_settings['steps'][$index] : false;
+    }
+
+    public function get_step_title_by_id( string $step_id ) {
+        $step = $this->get_step_by_id( $step_id );
+        return ! empty( $step['title'] ) ? $step['title'] : false;
+    }
+
+    public function generate_next_option_id( string $step_id ) {
+        $step = $this->get_step_by_id( $step_id );
+        if ( ! $step || empty( $step['options'] ) ) return;
+        $next_id = 0;
+        foreach ( $step['options'] as $option ) {
+            if ( $option['id'] >= $next_id ) 
+                $next_id = $option['id'];
+        }
+        return $next_id + 1;
+    }
+
+    public function get_option_index_by_id( string $step_id, $option_id ) {
+        $step = $this->get_step_by_id( $step_id );
+        if ( ! $step || empty( $step['options'] ) ) return;
+        $option_index = false;
+        foreach ( $step['options'] as $index => $option ) {
+            if ( $option['id'] == $option_id ) 
+                $option_index = $index;
+        }
+        return $option_index;
     }
 
 }
