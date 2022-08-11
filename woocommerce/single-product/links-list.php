7917d12d2cf204208812b3ce7ceb4bb4bb3be540
Robbert Vermeulen <robbertvermeulen@Robberts-MacBook-Pro-2.local>
2022-08-11 11:18:49 +0200

    Change assembly instruction icon and label in links list

--- woocommerce/single-product/links-list.php
+++ woocommerce/single-product/links-list.php
@@ -19,7 +19,7 @@ global $product; ?>
         <li class="links-list__item"><i class="fas fa-ruler-combined links-list__icon"></i> <a href="<?php echo $product->get_info_file_url( 'measurement' ); ?>" class="links-list__link" rel="nofollow" target="_blank"><?php _e( 'Meetinstructie', 'glasbestellen' ); ?></a></li>
     <?php } ?>
     <?php if ( $product->get_info_file_url( 'assembly' ) ) { ?>
-        <li class="links-list__item"><i class="fas fa-wrench links-list__icon"></i> <a href="<?php echo $product->get_info_file_url( 'assembly' ); ?>" class="links-list__link" rel="nofollow" target="_blank"><?php _e( 'Montageinstructie', 'glasbestellen' ); ?></a></li>
+        <li class="links-list__item"><i class="fas fa-download links-list__icon"></i> <a href="<?php echo $product->get_info_file_url( 'assembly' ); ?>" class="links-list__link" rel="nofollow" target="_blank"><?php _e( 'Download montagehandleiding', 'glasbestellen' ); ?></a></li>
     <?php } ?>
     <?php if ( $product->get_info_file_url( 'fittings' ) ) { ?>
         <li class="links-list__item"><i class="fas fa-info-circle links-list__icon"></i> <a href="<?php echo $product->get_info_file_url( 'fittings' ); ?>" class="links-list__link" rel="nofollow" target="_blank"><?php _e( 'Beslag informatie', 'glasbestellen' ); ?></a></li>
