aad500e04ffeab3da7d18d4cfde6a4b41252743d
Robbert Vermeulen <robbertvermeulen@Robberts-MacBook-Pro-2.local>
2022-08-11 11:21:17 +0200

    Change explanation link label in links list

--- woocommerce/single-product/links-list.php
+++ woocommerce/single-product/links-list.php
@@ -10,7 +10,7 @@ global $product; ?>
         <li class="links-list__item"><i class="fas fa-question-circle links-list__icon"></i> <a href="<?php echo $product->get_faq_page_url(); ?>" class="links-list__link" rel="nofollow" target="_blank"><?php _e( 'Veelgestelde vragen', 'glasbestellen' ); ?></a></li>
     <?php } ?>
     <?php if ( $product->get_explanation_page_url() ) { ?>
-        <li class="links-list__item"><i class="fas fa-cog links-list__icon"></i> <a href="<?php echo $product->get_explanation_page_url(); ?>" class="links-list__link" rel="nofollow" target="_blank"><?php _e( 'Uitleg stappen', 'glasbestellen' ); ?></a></li>
+        <li class="links-list__item"><i class="fas fa-cog links-list__icon"></i> <a href="<?php echo $product->get_explanation_page_url(); ?>" class="links-list__link" rel="nofollow" target="_blank"><?php _e( 'Uitleg stappen configurator', 'glasbestellen' ); ?></a></li>
     <?php } ?>
     <?php if ( $product->get_info_file_url( 'corrections' ) ) { ?>
         <li class="links-list__item"><i class="fas fa-arrows-alt-h links-list__icon"></i> <a href="<?php echo $product->get_info_file_url( 'corrections' ); ?>" class="links-list__link" rel="nofollow" target="_blank"><?php _e( 'Glascorrecties', 'glasbestellen' ); ?></a></li>
