727fd6656f8d512b67266d5aa959928726120003
Robbert Vermeulen <robbertvermeulen@Robberts-MacBook-Pro-2.local>
2022-08-24 14:39:04 +0200

    Reviews post type rewrite on has_archive property

---
+++ inc/widgets.php
@@ -0,0 +1,8 @@
+<?php
+// function gb_widgets_init() {
+//     register_sidebar([
+//         'id' => 'shop_category_sidebar',
+//         'name' => __( 'Shop category sidebar' )
+//     ]);
+// }
+// add_action( 'widgets_init', 'gb_widgets_init' );
