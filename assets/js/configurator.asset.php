54377063367da74bf72536e86c8befcab235f8d1
Robbert Vermeulen <robbertvermeulen@Robberts-MacBook-Pro-2.local>
2022-10-03 16:08:13 +0200

    Build

--- assets/js/configurator.asset.php
+++ assets/js/configurator.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-element'), 'version' => '41a0fc6f3e1c4364af63');
+<?php return array('dependencies' => array('lodash', 'wp-element'), 'version' => '0bab770abe01f1a2f341');
