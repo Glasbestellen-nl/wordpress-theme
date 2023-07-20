f4defc03e71e8119f8206175834ead0bb6d031d8
Robbert Vermeulen <robbertvermeulen@Robberts-MacBook-Pro.local>
2023-07-20 16:33:19 +0200

    Test remove javascript form validation general form

--- assets/js/main.asset.php
+++ assets/js/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-element'), 'version' => 'ac032b5adbedecdfe843');
+<?php return array('dependencies' => array('jquery', 'wp-element'), 'version' => '66d3e51d9e558443f16d');
