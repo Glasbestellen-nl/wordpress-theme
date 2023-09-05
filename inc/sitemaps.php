7cd103fc43108c722cdafee70fb668df095b5db1
Robbert Vermeulen <robbertvermeulen@Robberts-MacBook-Pro.local>
2023-09-05 13:55:28 +0200

    Debug

--- inc/sitemaps.php
+++ inc/sitemaps.php
@@ -1,15 +1,9 @@
 <?php
-/**
- * Exclude sitemaps
- */
 function sitemap_exclude_post_type( $value, $post_type ) {
   if ( $post_type == 'product' ) return true;
 }
 add_filter( 'wpseo_sitemap_exclude_post_type', 'sitemap_exclude_post_type', 10, 2 );
 
-/**
- * Change products sitemap
- */
 function gb_change_products_sitemap() {
   global $wpseo_sitemaps;
   if ( empty( $wpseo_sitemaps ) ) return;
@@ -17,9 +11,6 @@ function gb_change_products_sitemap() {
 }
 add_action( 'init', 'gb_change_products_sitemap', 99 );
 
-/**
- * Create products sitemap
- */
 function create_product_sitemap() {
 
   global $wpseo_sitemaps;
@@ -30,18 +21,9 @@ function create_product_sitemap() {
   ]);
   if ( empty( $products_query->terms ) ) return;
   $sitemap = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-
   foreach ( $products_query->terms as $term ) {
-
-    if ( get_field( 'hide_on_archive', 'term_' . $term->term_id ) ) 
-      continue;
-
+    if ( get_field( 'hide_on_archive', 'term_' . $term->term_id ) ) continue;
     $url = get_term_link( $term );
-    // $lastmod = get_term_meta( $term->term_id, 'last_modified_date', true ) ?? time();
-    //$lastmod = date( 'c', $lastmod );
-
-    //echo $lastmod;
-
     $sitemap .= '
       <url>
         <loc>' . $url . '</loc>
@@ -54,20 +36,14 @@ function create_product_sitemap() {
   $wpseo_sitemaps->set_sitemap( $sitemap );
 }
 
-/**
- * Create external sitemap
- */
 function gb_enable_custom_sitemap() {
-  global $wpseo_sitemaps;
-  if ( isset( $wpseo_sitemaps ) && ! empty ( $wpseo_sitemaps ) ) {
-    $wpseo_sitemaps->register_sitemap( 'external', 'create_external_sitemap' );
-  }
+   global $wpseo_sitemaps;
+   if ( isset( $wpseo_sitemaps ) && ! empty ( $wpseo_sitemaps ) ) {
+      $wpseo_sitemaps->register_sitemap( 'external', 'create_external_sitemap' );
+   }
 }
 add_action( 'init', 'gb_enable_custom_sitemap' );
 
-/**
- * Create products sitemap
- */
 function gb_add_sitemap_custom_items( $sitemap_custom_items ) {
    $sitemap_custom_items .= '
       <sitemap>
