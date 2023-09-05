ba78b26bfb5d72993630ce06823040465437c43d
Robbert Vermeulen <robbertvermeulen@Robberts-MacBook-Pro.local>
2023-09-05 15:48:10 +0200

    Debug

--- inc/sitemaps.php
+++ inc/sitemaps.php
@@ -38,10 +38,10 @@ function create_product_sitemap() {
 
     $url = get_term_link( $term );
     $lastmod = get_term_meta( $term->term_id, 'last_modified_date', true );
-    // if (!$lastmod) {
-    //   $lastmod = time();
-    // }
-    //$lastmod = date( 'c', $lastmod );
+    if (empty($lastmod)) {
+      $lastmod = time();
+    }
+    $lastmod = date( 'c', $lastmod );
 
     $sitemap .= '
       <url>
