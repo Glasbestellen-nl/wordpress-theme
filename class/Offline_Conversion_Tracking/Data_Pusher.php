e7aa26d123bf4267ede7fae22789b423e480377d
Robbert Vermeulen <robbertvermeulen@Robberts-MacBook-Pro.local>
2023-09-25 11:35:06 +0200

    Update data pusher seperate push meta

--- class/Offline_Conversion_Tracking/Data_Pusher.php
+++ class/Offline_Conversion_Tracking/Data_Pusher.php
@@ -9,29 +9,33 @@ class Data_Pusher {
       $conversions = $this->get_conversions();
       if (!$conversions) return;
 
-      $url = get_option('offline_conversion_webhook_url');
-      if (!$url) return;
+      $webhook_url = get_option('offline_conversion_webhook_url');
 
       foreach ($conversions as $conversion) {
 
          // Send data to Google Analytics 4 when client id is available
-         if (!empty($conversion['client_id'])) {
-            $this->send_offline_conversion_to_ga4($conversion);
+         if (!empty($conversion['client_id']) && !CRM::get_lead_meta( $lead->lead_id, 'conversion_data_pushed', true)) {
+            $success = $this->send_offline_conversion_to_ga4($conversion);
+            if ($success) {
+               CRM::update_lead_meta($conversion['lead_id'],  'conversion_data_pushed', 1);
+            }
          }
 
          // Make webhook call
-         $data = [
-            'lead_id' => $conversion['lead_id'],
-            'revenue' => $conversion['revenue'],
-            'shipping_price' => $conversion['shipping_price'],
-            'items' => $conversion['items'],
-            'client_id' => $conversion['client_id'] ?? null,
-            'gclid' => $conversion['gclid'] ?? null,
-            'timestamp' => $conversion['timestamp']
-         ];
-         $this->make_webhook_post_request($url, $data);
-
-         CRM::update_lead_meta( $conversion['lead_id'],  'conversion_data_pushed', 1 );
+         if ($webhook_url && !empty($conversion['gclid']) && !CRM::get_lead_meta( $lead->lead_id, 'ads_conversion_data_pushed', true)) {
+            $data = [
+               'lead_id' => $conversion['lead_id'],
+               'revenue' => $conversion['revenue'],
+               'shipping_price' => $conversion['shipping_price'],
+               'items' => $conversion['items'],
+               'gclid' => $conversion['gclid'],
+               'timestamp' => $conversion['timestamp']
+            ];
+            $success = $this->make_webhook_post_request($webhook_url, $data);
+            if ($success) {
+               CRM::update_lead_meta($conversion['lead_id'],  'ads_conversion_data_pushed', 1);
+            }
+         }
       }
       return $conversions;
    }
@@ -64,9 +68,11 @@ class Data_Pusher {
 
       $measurement_id = get_option('ga4_measurement_id');
       $api_secret = get_option('ga4_api_secret');
-      if (!$measurement_id || !$api_secret) return false;
+      if (empty($measurement_id) || empty($api_secret)) return false;
       $request_url = "https://www.google-analytics.com/mp/collect?measurement_id={$measurement_id}&api_secret={$api_secret}";
     
+      $items = [];
+
       if (!empty($conversion['items'])) {
          $items = array_map(function($item) {
             return [
@@ -129,25 +135,22 @@ class Data_Pusher {
 
       foreach ( $leads as $lead ) {
 
-         if (!CRM::get_lead_meta( $lead->lead_id, 'conversion_data_pushed', true)) {
+         $conversion_data = CRM::get_lead_meta( $lead->lead_id, 'conversion_data', true );
+         $client_id = CRM::get_lead_meta( $lead->lead_id, 'gclid', true );
+         $gclid = CRM::get_lead_meta( $lead->lead_id, 'ads_gclid', true );
 
-            $conversion_data = CRM::get_lead_meta( $lead->lead_id, 'conversion_data', true );
-            $client_id = CRM::get_lead_meta( $lead->lead_id, 'gclid', true );
-            $gclid = CRM::get_lead_meta( $lead->lead_id, 'ads_gclid', true );
+         if ($conversion_data && ($client_id || $gclid)) {
 
-            if ($conversion_data && ($client_id || $gclid)) {
-
-               $conversion = [
-                  'lead_id' => $lead->lead_id,
-                  'revenue' => $conversion_data['revenue'],
-                  'shipping_price' => ! empty( $conversion_data['shipping_price'] ) ? $conversion_data['shipping_price'] : 0,
-                  'items' => ! empty( $conversion_data['items'] ) ? $conversion_data['items'] : [],
-                  'client_id' => $client_id,
-                  'gclid' => $gclid,
-                  'timestamp' => time()
-               ];
-               $conversions[] = $conversion;
-            }
+            $conversion = [
+               'lead_id' => $lead->lead_id,
+               'revenue' => $conversion_data['revenue'],
+               'shipping_price' => ! empty( $conversion_data['shipping_price'] ) ? $conversion_data['shipping_price'] : 0,
+               'items' => ! empty( $conversion_data['items'] ) ? $conversion_data['items'] : [],
+               'client_id' => $client_id,
+               'gclid' => $gclid,
+               'timestamp' => time()
+            ];
+            $conversions[] = $conversion;
          }
       }
       return $conversions;
