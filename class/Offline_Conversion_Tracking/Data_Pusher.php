081604361dc6dc1cb661e8d115cba490e323654d
Robbert Vermeulen <robbertvermeulen@Robberts-MacBook-Pro.local>
2023-09-29 10:12:42 +0200

    Debug data pusher

--- class/Offline_Conversion_Tracking/Data_Pusher.php
+++ class/Offline_Conversion_Tracking/Data_Pusher.php
@@ -15,9 +15,7 @@ class Data_Pusher {
 
          // Send data to Google Analytics 4 when client id is available
          if (!empty($conversion['client_id']) && !CRM::get_lead_meta( $conversion['lead_id'], 'conversion_data_pushed', true)) {
-            var_dump($conversion['client_id']);
             $success = $this->send_offline_conversion_to_ga4($conversion);
-            var_dump($success);
             if ($success) {
                CRM::update_lead_meta($conversion['lead_id'],  'conversion_data_pushed', 1);
             }
@@ -114,7 +112,6 @@ class Data_Pusher {
             return false;
          }
          $response_code = wp_remote_retrieve_response_code( $response );
-         var_dump($response_code);
          if (in_array($response_code, [200, 201, 204])) {
             return true;
          }
